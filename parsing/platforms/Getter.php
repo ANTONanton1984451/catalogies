83830ca689a85d9ba7e08578895dc74c6945f888
Kirill Zaytcev <borland667@mail.ru>
2020-07-29 15:24:48 +0400

    Конфигурация dev-версии проекта

--- parsing/platforms/Getter.php
+++ parsing/platforms/Getter.php
@@ -4,5 +4,25 @@ namespace parsing\platforms;
 
 abstract class Getter
 {
+    protected $source;
+    protected $actual;
+    protected $track;
 
+    public function setSource($source)
+    {
+        $this->source = $source;
+    }
+
+    public function setActual($actual)
+    {
+        $this->actual = $actual;
+    }
+
+    public function setTrack($track)
+    {
+        $this->track = $track;
+    }
+
+    public abstract function getNextReview();
+    public abstract function getNotifications();
 }
