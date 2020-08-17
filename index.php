6357b7051db02796176ff646aad39368d21c2f7c
Kirill Zaytcev <borland667@mail.ru>
2020-08-17 14:07:39 +0400

    s

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use parsing\ParserManager;
 use parsing\platforms\zoon\ZoonGetter;
-use parsing\platforms\zoon\ZoonFilter2;
+use parsing\platforms\zoon\ZoonFilter;
 use parsing\platforms\zoon\ZoonModel;
 use parsing\DB\DatabaseShell;
 
@@ -13,28 +13,10 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-//$manager = new ParserManager();
-//$manager->parseSources();
+$manager = new ParserManager();
+$manager->parseSources();
 
 //setSource();
-
-$config = (new DatabaseShell())->getActualSourceReviews();
-$config = $config[0];
-
-$getter = new ZoonGetter();
-$filter = new ZoonFilter2();
-$model = new ZoonModel();
-
-$getter->setConfig($config);
-$filter->setConfig($config);
-$model->setConfig($config);
-
-$buffer = $getter->getNextRecords();
-$buffer = $getter->getNextRecords();
-$buffer = $getter->getNextRecords();
-
-$buffer = $filter->clearData($buffer);
-//$model->writeData($buffer);
 
 function setSource() {
     $source = 'https://volgograd.zoon.ru/restaurants/kapuchino_v_krasnooktyabrskom_rajone/';
