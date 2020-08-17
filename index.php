dbc57bf19a4894cc5000e3a0aed6ee01e92505aa
Kirill Zaytcev <borland667@mail.ru>
2020-08-17 10:19:10 +0400

    And something else

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php
 
 use parsing\ParserManager;
+use parsing\platforms\zoon\ZoonGetter;
+use parsing\platforms\zoon\ZoonFilter2;
+use parsing\platforms\zoon\ZoonModel;
+use parsing\DB\DatabaseShell;
 
 require_once "autoloader.php";
 require_once "vendor/autoload.php";
@@ -12,10 +16,28 @@ ini_set('display_startup_errors', 1);
 //$manager = new ParserManager();
 //$manager->parseSources();
 
-setSource();
+//setSource();
+
+$config = (new DatabaseShell())->getActualSourceReviews();
+$config = $config[0];
+
+$getter = new ZoonGetter();
+$filter = new ZoonFilter2();
+$model = new ZoonModel();
+
+$getter->setConfig($config);
+$filter->setConfig($config);
+$model->setConfig($config);
+
+$buffer = $getter->getNextRecords();
+$buffer = $getter->getNextRecords();
+$buffer = $getter->getNextRecords();
+
+$buffer = $filter->clearData($buffer);
+//$model->writeData($buffer);
 
 function setSource() {
-    $source = 'https://volgograd.zoon.ru/shops/kulinariya_konfetki-baranochki_na_prospekte_lenina-9fcd/';
+    $source = 'https://volgograd.zoon.ru/restaurants/kapuchino_v_krasnooktyabrskom_rajone/';
     $db = new \parsing\DB\DatabaseShell();
     $db->insertSourceReview([
         'source_hash'   =>  md5($source),
