5280febf087c53e338e58821eef182681fcd4ae4
ANTONanton1984 <anton.shushp@yandex.ru>
2020-08-25 09:12:56 +0300

    Working with Logger

--- index.php
+++ index.php
@@ -4,7 +4,9 @@ use parsing\ParserManager;
 use parsing\platforms\zoon\ZoonGetter;
 use parsing\platforms\zoon\ZoonFilter2;
 use parsing\platforms\zoon\ZoonModel;
-
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Monolog\Handler\FirePHPHandler;
 
 require_once "autoloader.php";
 require_once "vendor/autoload.php";
@@ -14,6 +16,17 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 
+//$logger = new Logger('dev');
+//// Now add some handlers
+//$logger->pushHandler(new StreamHandler(__DIR__.'/my_dev.log', Logger::DEBUG));
+//$logger->pushHandler(new FirePHPHandler());
+//
+//// You can now use your logger
+//$logger->
 
-(new ParserManager())->parseSources();
+\parsing\LoggerManager::init();
 
+\parsing\LoggerManager::log('test','test',['data'=>'test']);
+
+['source'=>'',
+  'config'=>['token_info'=>['конфиги токена']]  ];
