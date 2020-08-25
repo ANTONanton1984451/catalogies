c3fa165b73a90a426b284305a1090efda93fcbda
Kirill Zaytcev <borland667@mail.ru>
2020-08-25 09:05:48 +0400

    temp zoon

--- index.php
+++ index.php
@@ -1,28 +1,50 @@
 <?php
 
-use parsing\ParserManager;
-
-require_once "autoloader.php";
 require_once "vendor/autoload.php";
+require_once "autoloader.php";
+
+use parsing\DB\DatabaseShell;
+use Workerman\Worker;
+use Workerman\Timer;
+use parsing\ParserManager;
 
 ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
-$manager = new ParserManager();
-$manager->parseSources();
+loopGo();
 
-//setSource();
+//setSource(['https://volgograd.zoon.ru/restaurants/bar_vedrov/']);
 
-function setSource() {
-    $source = 'https://volgograd.zoon.ru/restaurants/kafe_nosorog/';
-    $db = new \parsing\DB\DatabaseShell();
-    $db->insertSourceReview([
-        'source_hash'   =>  md5($source),
-        'platform'      =>  'zoon',
-        'source'        =>  $source,
-        'actual'        =>  'ACTIVE',
-        'track'         =>  'ALL',
-        'handled'       =>  'NEW'
-    ]);
+function setSource(array $sources)
+{
+    foreach ($sources as $source) {
+        $db = new DatabaseShell();
+        $db->insertSourceReview([
+            'source_hash' => md5($source),
+            'platform' => 'zoon',
+            'source' => $source,
+            'actual' => 'ACTIVE',
+            'track' => 'ALL',
+            'handled' => 'NEW'
+        ]);
+    }
+}
+
+function loopGo() {
+//    $worker = new Worker();
+//    $worker->count = 1;
+//    $worker->name = "Worker for catalogs module";
+//    $worker->onWorkerStart = (function (){
+//        $timeInterval = 20;
+//        $timerId = Timer::add($timeInterval, function () {
+
+        $manager = new ParserManager();
+        $manager->parseSources();
+//
+//        });
+//    });
+//
+//    Worker::runAll();
+
 }
